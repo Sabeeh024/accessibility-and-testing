e1dedd402ee24b34b59f8d39028e1a17ffa0d36b
Sabeeh024 <sabeehjavaid024@gmail.com>
2026-09-15 06:59:34 +0500

    Initial pristine Vite+React scaffold

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Accessibility &amp; Testing scaffold</h1>
+      <p>Pristine base for topic branches. See NOTES/ on master for the curriculum.</p>
+    </main>
+  )
+}
+
+export default App
